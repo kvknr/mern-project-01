30a7839074eca194cc57a530c8ceac64c50881db
Vinoth Kumar <kumarvinothknr@gmail.com>
2022-10-22 07:44:01 +0530

    Pages and Routes are Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+
 import Users from "./user/pages/Users";
 
 function App() {
-  return <Users />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new">
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
